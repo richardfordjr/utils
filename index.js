f4d0c4df66122a81d11650a0b9e353c3a6c8fb8d
Richard Ford <richard.ford@thyssenkrupp.com>
2017-06-06 20:23:34 -0500

    Updates

--- index.js
+++ index.js
@@ -1,30 +1,9 @@
 'use strict';
 
-var Promise = require('bluebird'),
-request = require('request'),
-methods = require('./lib/methods.js');
-
-
 /**
  * Module exports.
  * @public
  */
-
-
-
-console.log('app started');
-exports.getData = function (url, method){
-  var opts = {
-    url: url,
-    method: methods[method]
-  };
-  return new Promise(function(resolve, reject){
-     request(opts,function(err,res){
-       if(err){
-         reject(err);
-       }else{
-         resolve(res);
-       }
-     })
-  })
+exports.add = function (a,b){
+ return a + b;
 }
