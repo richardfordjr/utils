c73a17d73dd904fab136ff13ecf881e8e208979d
Richard Ford <richard.ford@thyssenkrupp.com>
2017-06-06 20:48:06 -0500

    Updates

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 'use strict';
+var request = require('request');
+var Promise = require('bluebird');
 
 /**
  * Module exports.
  * @public
  */
-exports.add = function (a,b){
+ exports.add = add;
+
+function add(a,b){
  return a + b;
 }
